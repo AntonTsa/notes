563b79b06ec73c596a7603ae1d0d26de1dbe45e7
Anton Tsarenko <itantontsarenko@gmail.com>
2025-12-18 16:53:50 +0200

    feat: Added Flyway Migrations

---
+++ src/main/resources/db/migration/V2__populate_db.sql
@@ -0,0 +1,5 @@
+INSERT INTO NOTE (ID, TITLE, CONTENT) VALUES
+(1, 'First Note', 'My First Note'),
+(2, 'My Second Note', 'I use Java'),
+(3, 'My Today', 'I do the task'),
+(4, 'Call Peter', 'Ask Peter if is all right!');
